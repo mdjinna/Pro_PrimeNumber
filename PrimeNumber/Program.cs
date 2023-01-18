0f55810e237e6d21429f148a31e97638a4fc5297
mdjinna <edmdjinna2000@gmail.com>
2023-01-18 17:52:07 +0530

    Add project files.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get Input from Console
+            Console.Write("Enter value to show prime number : ");
+            int InputVal = int.Parse(Console.ReadLine());
+            bool PrimeYes;
+            for (int primeloop = 2; primeloop <= InputVal; primeloop++)
+            {
+                PrimeYes = true;
+                for (int divvalues = 2; divvalues <= (primeloop / 2); divvalues++)
+                {
+                    if (primeloop % divvalues == 0)
+                    {
+                        PrimeYes = false;
+                        break;
+                    }
+                }
+                if (PrimeYes)
+                {
+                    //Print prime number
+                    Console.WriteLine(primeloop);
+                }
+            }
+            // Tohold screen
+            Console.ReadKey();
+        }
+    }
+}
